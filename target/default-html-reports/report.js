f7a0d511bb441615b1370270129bf1e3fc95a6a9
meferyilmaz <99425043+meferyilmaz@users.noreply.github.com>
2022-11-30 19:37:38 +0300

    first commit

---
+++ target/default-html-reports/report.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/Contacts.feature");
+formatter.feature({
+  "name": "Contacts page",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Contact test with email",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@db"
+    },
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logged in as \"sales manager\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logged_in_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Customers\" \"Contacts\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.NavigationMenuStepDefs.the_user_navigates_to(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks the \"mrjakc@mail.ru\" from contacts",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_clicks_the_from_contacts(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the information should be same with database",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_information_should_be_same_with_database()"
+});
+formatter.result({
+  "error_message": "java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0\r\n\tat java.base/jdk.internal.util.Preconditions.outOfBounds(Preconditions.java:64)\r\n\tat java.base/jdk.internal.util.Preconditions.outOfBoundsCheckIndex(Preconditions.java:70)\r\n\tat java.base/jdk.internal.util.Preconditions.checkIndex(Preconditions.java:248)\r\n\tat java.base/java.util.Objects.checkIndex(Objects.java:372)\r\n\tat java.base/java.util.ArrayList.get(ArrayList.java:459)\r\n\tat com.vytrack.utilities.DBUtils.getRowMap(DBUtils.java:77)\r\n\tat com.vytrack.step_definitions.ContactsStepDefs.the_information_should_be_same_with_database(ContactsStepDefs.java:100)\r\n\tat ✽.the information should be same with database(file:///C:/Users/KAAN%20EFE/IdeaProjects/EU8-Vytrack-DB/src/test/resources/features/Contacts.feature:69)\r\n",
+  "status": "failed"
+});
+formatter.embedding("image/png", "embedded0.png", "screenshot");
+formatter.after({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
